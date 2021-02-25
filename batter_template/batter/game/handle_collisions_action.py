7d5c650e72bd5f663f5f65d89426ec576d7632f8
Chandler Owen <chantheman29@icloud.com>
2021-02-25 14:05:40 -0600

    work on the walls

--- batter_template/batter/game/handle_collisions_action.py
+++ batter_template/batter/game/handle_collisions_action.py
@@ -17,19 +17,32 @@ class HandleCollisionsAction(Action):
             cast (dict): The game actors {key: tag, value: list}.
         """
         ball = cast["ball"][0] # There's only one ball
-        brick = cast["brick"]
+        brick = cast["brick"] # there's multiple bricks
+        paddle = cast["paddle"][0] #  there's only one paddle
+
+        #  check if the ball hits the paddle
+        for i in range(11):
+            x = paddle._position.get_x()
+            y = paddle._position.get_y()
+            position = Point(x+i, y)
+            if ball.get_position().equals(position):
+                v = random.randint(-2,2)
+                v_y = ball._velocity.get_y()
+                velocity = Point(v,v_y*-1)
+                ball.set_velocity(velocity)
+
+        #  Check if the brick is hit and remove it
         for i in brick:
             if ball.get_position().equals(i.get_position()):
                 brick.remove(i)
-                velocity = Point(-1, 1)
+                v = random.randint(-2,2)
+                v_y = ball._velocity.get_y()
+                velocity = Point(v,v_y*-1)
                 ball.set_velocity(velocity)
 
-        
-        # marquee = cast["marquee"][0] # there's only one
-        # robot = cast["robot"][0] # there's only one
-        # artifacts = cast["artifact"]
-        # marquee.set_text("")
-        # for artifact in artifacts:
-        #     if robot.get_position().equals(artifact.get_position()):
-        #         description = artifact.get_description()
-        #         marquee.set_text(description) 
+        #  check if the ball hits the bottom of the screen
+        # if ball._position.get_y() == 20:
+        #     position = Point(0,0)
+        #     ball.set_position(position)
+        #     velocity = Point(0,0)
+        #     ball.sett_velocity(velocity)
