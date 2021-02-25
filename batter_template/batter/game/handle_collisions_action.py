3751b1587f0f871187268915cad11c3366070632
Chandler Owen <chantheman29@icloud.com>
2021-02-25 17:42:09 -0600

    only game over screen and comments needed now

--- batter_template/batter/game/handle_collisions_action.py
+++ batter_template/batter/game/handle_collisions_action.py
@@ -14,7 +14,7 @@ class HandleCollisionsAction(Action):
         """Executes the action using the given actors.
 
         Args:
-            cast (dict): The game actors {key: tag, value: list}.
+            cast (dict): The game actors {key: value: list}.
         """
         ball = cast["ball"][0] # There's only one ball
         brick = cast["brick"] # there's multiple bricks
@@ -26,7 +26,7 @@ class HandleCollisionsAction(Action):
             y = paddle._position.get_y()
             position = Point(x+i, y)
             if ball.get_position().equals(position):
-                v = random.randint(-2,2)
+                v = random.randint(-1,1)
                 v_y = ball._velocity.get_y()
                 velocity = Point(v,v_y*-1)
                 ball.set_velocity(velocity)
@@ -35,14 +35,30 @@ class HandleCollisionsAction(Action):
         for i in brick:
             if ball.get_position().equals(i.get_position()):
                 brick.remove(i)
-                v = random.randint(-2,2)
+                v = random.randint(-1,1)
                 v_y = ball._velocity.get_y()
                 velocity = Point(v,v_y*-1)
                 ball.set_velocity(velocity)
 
+        # check if the ball hits the top of the screen
+        if ball._position.get_y() == 1:
+            v_x = ball._velocity.get_x()
+            v_y = ball._velocity.get_y()
+            velocity = Point(v_x, -1*v_y)
+            ball.set_velocity(velocity)
+
+        # check if the ball hits the sides of the screen
+        if (ball._position.get_x() == (constants.MAX_X - 4)) | (ball._position.get_x() == 2):
+            v_x = ball._velocity.get_x()
+            v_y = ball._velocity.get_y()
+            velocity = Point(v_x*-1, v_y)
+            ball.set_velocity(velocity)
+
         #  check if the ball hits the bottom of the screen
-        # if ball._position.get_y() == 20:
-        #     position = Point(0,0)
-        #     ball.set_position(position)
-        #     velocity = Point(0,0)
-        #     ball.sett_velocity(velocity)
+        if ball._position.get_y() == (constants.MAX_Y - 1):
+            # v = random.randint(-1,1)
+            # v_y = ball._velocity.get_y()
+            velocity = Point(0,0)
+            ball.set_velocity(velocity)
+            # add game over to the screen
+
